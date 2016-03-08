8416ab01cefd53a50fabcfc72c384d24cd2df9a6
张韧 <zhangren@zhangrendeMacBook-Pro.local>
2016-03-08 16:33:06 +0800

    main

--- www/src/main/mainCtrl.js
+++ www/src/main/mainCtrl.js
@@ -4,10 +4,25 @@
 'use strict';
 mainModule
     .controller('MainCtrl', ['$scope', 'ionicMaterialInk', function ($scope, ionicMaterialInk) {
-        var today = new Date().getDay();
-        console.log(today)
+
+
 
         ionicMaterialInk.displayEffect();
+        /*--------------------------------------------日历--------------------------------------------*/
+        //判断是否是闰年,2月平年28天、闰年29天
+        var isLeapYear = function(year)
+        {
+            return ((year%4==0&&year%100!=0)||year%400==0);
+        }
+        var mydate = new Date();
+        //当天周几
+        var today = mydate.getDay();
+        //当天多少号
+        var day = mydate.getDate();
+        var year = mydate.getFullYear();
+        var month = mydate.getMonth()+1;
+        console.log(year+'----'+month+'----'+day+'----'+isLeapYear(year))
+
         $scope.weeks = [{value: '一', flag: false}, {value: '二', flag: false}, {value: '三', flag: false
         }, {value: '四', flag: false}, {value: '五', flag: false}, {value: '六', flag: false}, {value: '日', flag: false}];
         $scope.weeks[today-1].today = '今';
@@ -17,4 +32,6 @@ mainModule
             }
             x.flag = true;
         }
+
+        /*-------------------------------------------日历 end-------------------------------------------*/
     }]);
