74028579730ca632197f9c4a03bd7e5fed251e2b
zhangren <zhangren@zhangrendeMacBook-Pro.local>
2016-03-17 17:38:50 +0800

    应用页

--- www/src/applications/applications.js
+++ www/src/applications/applications.js
@@ -6,4 +6,24 @@ appModule
     .controller('AppCtrl',['$scope','$state','ionicMaterialInk',function($scope,$state,ionicMaterialInk){
         console.log('app')
         ionicMaterialInk.displayEffect();
+
+        $scope.imgs = [{
+            url:'../../img/apps/partner.png'
+        },{
+            url:'../../img/apps/saleAct.png'
+        },{
+            url:'../../img/apps/acPlan.png'
+        },{
+            url:'../../img/apps/saleChance.png'
+        },{
+            url:'../../img/apps/saleClue.png'
+        },{
+            url:'../../img/apps/saleQuote.png'
+        },{
+            url:'../../img/apps/sparePart.png'
+        },{
+            url:'../../img/apps/empInfo.png'
+        },{
+            url:'../../img/apps/searchCar.png'
+        }]
     }]);
