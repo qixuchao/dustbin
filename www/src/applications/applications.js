7924fb08aaf993a3f54ec07fe6f4e8df2b98c59f
Admin <admin@server.local>
2016-03-29 17:03:54 +0800

    修改工单

--- www/src/applications/applications.js
+++ www/src/applications/applications.js
@@ -29,7 +29,7 @@ appModule
             go:'spareList'
         },{
             url:'img/apps/empInfo.png',
-            go:'worksheetCarMileage'
+            go:'userQuery'
         },{
             url:'img/apps/searchCar.png',
             go:'car'
