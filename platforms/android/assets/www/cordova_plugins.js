31c8297d6c176117a59978857583991e53d8056d
xueboren <xueboren001@outlook.com>
2016-03-30 11:03:44 +0800

    crosswalk

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -3,6 +3,7 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-dialogs/www/notification.js",
         "id": "cordova-plugin-dialogs.notification",
+        "pluginId": "cordova-plugin-dialogs",
         "merges": [
             "navigator.notification"
         ]
@@ -10,15 +11,34 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
         "id": "cordova-plugin-dialogs.notification_android",
+        "pluginId": "cordova-plugin-dialogs",
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-dialogs": "1.2.0"
+    "cordova-plugin-crosswalk-webview": "1.6.1",
+    "cordova-plugin-dialogs": "1.2.0",
+    "cordova-plugin-inappbrowser": "1.3.0",
+    "cordova-plugin-whitelist": "1.2.1"
 }
 // BOTTOM OF METADATA
 });
