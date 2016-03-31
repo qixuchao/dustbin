da49965055b99bc422408703f4be5573b805e26d
zhangren <zhangren@zhangrendeMacBook-Pro.local>
2016-03-31 19:27:51 +0800

    .

--- www/src/public/CRMApp.js
+++ www/src/public/CRMApp.js
@@ -43,12 +43,15 @@ CRMApp.run(function ($ionicPlatform,$rootScope) {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+                cordova.plugins.Keyboard.disableScroll(true);
             }
-            if (window.StatusBar) {
-                // org.apache.cordova.statusbar required
-                StatusBar.styleLightContent();
-            }
+            //if (window.StatusBar) {
+            //    // org.apache.cordova.statusbar required
+            //    window.StatusBar.overlaysWebView(true);
+            //}
+            window.StatusBar.overlaysWebView(true);
+            window.StatusBar.style(1);
         });
         $rootScope.goState = function(state){
             $state.go(state);
