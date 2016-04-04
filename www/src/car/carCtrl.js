5ee713f7d1ba79295b2634f6567bc9b88f8f12bd
qixuchao <xuchao.qi@hand-china.com>
2016-04-04 10:12:31 +0800

    filter

--- www/src/car/carCtrl.js
+++ www/src/car/carCtrl.js
@@ -471,25 +471,4 @@ carModule.controller('CarCtrl',['$scope','CarService','$timeout','$state','Promp
             i+=1;
             $scope.spareList.push(spareInfo1);
         };
-}])
-.filter("highlight", ['$sce','$log',function($sce, $log){
-
-    var fn = function(text, search){
-        $log.info("text: " + text);
-        $log.info("search: " + search);
-
-        if (!search) {
-            return $sce.trustAsHtml(text);
-        }
-        text = encodeURI(text);
-        search = encodeURI(search);
-
-        var regex = new RegExp(search, 'gi')
-        var result = text.replace(regex, '<span style="color: red;">$&</span>');
-        result = decodeURI(result);
-        $log.info("result: " + result );
-        return $sce.trustAsHtml(result);
-    };
-
-    return fn;
 }]);
