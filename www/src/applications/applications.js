387b4cf8acc5ecf9f76927d32fa85cba76daaabe
Admin <admin@server.local>
2016-04-04 17:20:48 +0800

    修改工单页面

--- www/src/applications/applications.js
+++ www/src/applications/applications.js
@@ -29,7 +29,7 @@ appModule
             go:'spareList'
         },{
             url:'img/apps/empInfo.png',
-            go:'worksheetRelatedPart'
+            go:'worksheetCarMileage'
         },{
             url:'img/apps/searchCar.png',
             go:'car'
