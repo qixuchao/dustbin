205d9929952861991ab4f2f687d8e21049cdcaf8
Admin <admin@server.local>
2016-04-07 13:50:57 +0800

    报工单修改

--- www/src/public/CRMApp.js
+++ www/src/public/CRMApp.js
@@ -19,6 +19,7 @@ var customerContactsModule = angular.module('customerContactsModule', []);
 var customerModuleServive = angular.module('customerModuleServive', []);
 var spareModule = angular.module('spareModule',[]);
 var worksheetModule = angular.module('worksheetModule', []); // 工单模块
+var worksheetReportModule = angular.module('worksheetReportModule', []);
 
 
 var CRMApp = angular.module('CRMApp', ['ngAnimate', 'ionic', 'ngCordova',
@@ -44,7 +45,8 @@ var CRMApp = angular.module('CRMApp', ['ngAnimate', 'ionic', 'ngCordova',
     'customerChanceModule',
     'customerkeyModule',
     'customerActivityModule',
-    'customerWorkorderModule'
+    'customerWorkorderModule',
+    "worksheetReportModule"
 ]);
 CRMApp.run(function ($ionicPlatform,$rootScope, $ionicHistory) {
         $ionicPlatform.ready(function () {
@@ -321,11 +323,11 @@ CRMApp.run(function ($ionicPlatform,$rootScope, $ionicHistory) {
                 controller: 'WorksheetListReportedCtrl'
             })
 
-            //.state('worksheetReportedDetail', { // detailType取值: newCar、siteRepair、batchUpdate
-            //    url: '/worksheetDetail/{detailType}',
-            //    templateUrl: 'src/worksheet/detailAll/detailAll.html',
-            //    controller: 'worksheetDetailAllCtrl'
-            //})
+            .state('worksheetReportedDetail', {
+                url: '/worksheetReportedDetail',
+                templateUrl: 'src/worksheetReported/worksheetReportedDetail/worksheetReporteDetail.html',
+                controller: 'WorksheetListReportedDetailCtrl'
+            })
             .state('worksheetReportedInfosList', {
                 url: '/worksheetReportedInfosList',
                 templateUrl: 'src/worksheetReported/worksheetReportedInfos/worksheetReportedInfosList.html',
