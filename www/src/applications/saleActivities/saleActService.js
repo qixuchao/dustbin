89f75435572f9dd15586be1f9d69e91be05759f2
zhangren <ren.zhang@hand-china.com>
2016-04-11 21:45:42 +0800

    主页以及销售活动

--- www/src/applications/saleActivities/saleActService.js
+++ www/src/applications/saleActivities/saleActService.js
@@ -5,45 +5,45 @@
 salesModule.factory('saleActService', function () {
 
     var saleListArr2 = [{
-        title:'福州龙福汽车交流活动',
-        customer:'龙福汽车',
-        place:'福建省 福州市',
-        startTime:'2016-3-9',
-        de_startTime:'2016/3/9 12:00',
-        de_endTime:'2016/4/2 15:03',
-        type:'业务交流',
-        status:'处理中',
-        saleNum:'100036',
-        annotation:'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among',
-        refer:'商机-郑州客车销售机会',
-        progressArr:[{
+        title: '福州龙福汽车交流活动',
+        customer: '龙福汽车',
+        place: '福建省 福州市',
+        startTime: '2016-3-9',
+        de_startTime: '2016/3/9 12:00',
+        de_endTime: '2016/4/2 15:03',
+        type: '业务交流',
+        status: '处理中',
+        saleNum: '100036',
+        annotation: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among',
+        refer: '商机-郑州客车销售机会',
+        progressArr: [{
             content: '与客户进行了初次交涉,效果良好',
             time: '2016-3-6  18:33'
         }],
-        relations:[{
-            name:'王紫薇',
-            sex:'女士',
-            position:'CATL销售'
+        relations: [{
+            name: '王紫薇',
+            sex: '女士',
+            position: 'CATL销售'
 
-        },{
-            name:'克克',
-            sex:'男士',
-            position:'汉得CEO'
+        }, {
+            name: '克克',
+            sex: '男士',
+            position: '汉得CEO'
 
         }]
-    },{
-        title:'郑州金龙客车(福州)客户拜访',
-        customer:'金龙客车',
-        place:'福建省 福州市',
-        startTime:'2016-2-25',
-        de_startTime:'2016/3/1 12:00',
-        de_endTime:'2016/4/1 15:03',
-        type:'事务性活动',
-        status:'未处理',
-        saleNum:'100034',
-        annotation:'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among',
-        refer:'商机-郑州客车销售机会',
-        progressArr:[{
+    }, {
+        title: '郑州金龙客车(福州)客户拜访',
+        customer: '金龙客车',
+        place: '福建省 福州市',
+        startTime: '2016-2-25',
+        de_startTime: '2016/3/1 12:00',
+        de_endTime: '2016/4/1 15:03',
+        type: '事务性活动',
+        status: '未处理',
+        saleNum: '100034',
+        annotation: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among',
+        refer: '商机-郑州客车销售机会',
+        progressArr: [{
             content: '与客户进行了初次交涉,效果良好',
             time: '2016-3-6  18:33'
         }, {
@@ -53,30 +53,30 @@ salesModule.factory('saleActService', function () {
             content: '最后谈了一次,应该可以成交,主要联系客户李经理进行跟进',
             time: '2016-3-8  12:11'
         }],
-        relations:[{
-            name:'王紫薇',
-            sex:'女士',
-            position:'CATL销售'
+        relations: [{
+            name: '王紫薇',
+            sex: '女士',
+            position: 'CATL销售'
 
-        },{
-            name:'克克',
-            sex:'男士',
-            position:'汉得CEO'
+        }, {
+            name: '克克',
+            sex: '男士',
+            position: '汉得CEO'
 
         }]
-    },{
-        title:'这个周末清明节加班',
-        customer:'新能源项目组',
-        place:'上海市',
-        startTime:'2016-4-4',
-        de_startTime:'2016/3/1 12:00',
-        de_endTime:'2016/4/1 15:03',
-        type:'放假性活动',
-        status:'已完成',
-        saleNum:'120036',
-        annotation:'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among',
-        refer:'商机-郑州客车销售机会',
-        progressArr:[{
+    }, {
+        title: '这个周末清明节加班',
+        customer: '新能源项目组',
+        place: '上海市',
+        startTime: '2016-4-4',
+        de_startTime: '2016/3/1 12:00',
+        de_endTime: '2016/4/1 15:03',
+        type: '放假性活动',
+        status: '已完成',
+        saleNum: '120036',
+        annotation: 'In the tumultuous business of cutting-in and attending to a whale, there is much running backwards and forwards among',
+        refer: '商机-郑州客车销售机会',
+        progressArr: [{
             content: '加班第一天',
             time: '2016-4-2  18:33'
         }, {
@@ -86,34 +86,34 @@ salesModule.factory('saleActService', function () {
             content: '加班第二天',
             time: '2016-4-4  12:11'
         }],
-        relations:[{
-            name:'王紫薇',
-            sex:'女士',
-            position:'CATL销售'
+        relations: [{
+            name: '王紫薇',
+            sex: '女士',
+            position: 'CATL销售'
 
-        },{
-            name:'克克',
-            sex:'男士',
-            position:'汉得CEO'
+        }, {
+            name: '克克',
+            sex: '男士',
+            position: '汉得CEO'
 
         }]
     }];
     var saleListArr = [];
     var createPopTypes = [{
-       text:'业务交流'
-    },{
-        text:'事务性活动'
-    },{
-        text:'关系维护'
-    },{
-        text:'技术交流'
+        text: '业务交流'
+    }, {
+        text: '事务性活动'
+    }, {
+        text: '关系维护'
+    }, {
+        text: '技术交流'
     }];
     var createPopOrgs = [{
-       text:'公司间'
-    },{
-        text:'内销'
-    },{
-        text:'外销'
+        text: '公司间'
+    }, {
+        text: '内销'
+    }, {
+        text: '外销'
     }];
     var customerTypesArr = [{
         text: '正式客户'
@@ -121,10 +121,8 @@ salesModule.factory('saleActService', function () {
         text: '潜在客户'
     }, {
         text: '竞争对手'
-    },{
+    }, {
         text: '合作伙伴'
-    },{
-        text: '联系人'
     }];
     var customer = [{
         text: '郑州金龙客车(福州)分公司'
@@ -136,21 +134,21 @@ salesModule.factory('saleActService', function () {
         text: '测试2'
     }];
     var contacts = [{
-       text:'联系人1'
-    },{
-        text:'联系人2'
-    },{
-        text:'联系人3'
-    },{
-        text:'联系人4'
-    },{
-        text:'联系人5'
-    },{
-        text:'联系人6'
-    },{
-        text:'联系人7'
-    },{
-        text:'联系人8'
+        text: '联系人1'
+    }, {
+        text: '联系人2'
+    }, {
+        text: '联系人3'
+    }, {
+        text: '联系人4'
+    }, {
+        text: '联系人5'
+    }, {
+        text: '联系人6'
+    }, {
+        text: '联系人7'
+    }, {
+        text: '联系人8'
     }];
     var actDetail = {};
     var relationsPopArr = [{
@@ -166,23 +164,24 @@ salesModule.factory('saleActService', function () {
     }, {
         text: '合作伙伴'
     }];
-    var relationSelections=[{
-       name:'马云',
-        sex:'男士',
-       position:'后勤部长'
-    },{
-        name:'马化腾',
-        sex:'男士',
-        position:'保洁'
-    },{
-        name:'李彦宏',
-        sex:'女士',
-        position:'保安'
+    var relationSelections = [{
+        name: '马云',
+        sex: '男士',
+        position: '后勤部长'
+    }, {
+        name: '马化腾',
+        sex: '男士',
+        position: '保洁'
+    }, {
+        name: '李彦宏',
+        sex: '女士',
+        position: '保安'
     }];
-    var customerArr=[];
-    return{
+    var customerArr = [];
+    var listPage = 0;
+    return {
         getSaleListArr: function () {
-          return saleListArr2;
+            return saleListArr2;
         },
         saleListArr,
         getCreatePopTypes: function () {
@@ -201,12 +200,13 @@ salesModule.factory('saleActService', function () {
             return contacts;
         },
         actDetail,
-        getRelationsPopArr : function () {
+        getRelationsPopArr: function () {
             return relationsPopArr;
         },
         getRelationSelections: function () {
             return relationSelections;
         },
-        customerArr
+        customerArr,
+        listPage
     };
 });
