61b3b48ea7a6b439881d0e1ce410fc3c967c0692
Admin <admin@server.local>
2016-04-13 22:18:03 +0800

    车辆里程 故障 相关方等接口

--- www/src/worksheet/carMileage/worksheetCarMileageCtr.js
+++ www/src/worksheet/carMileage/worksheetCarMileageCtr.js
@@ -3,24 +3,65 @@ worksheetModule.controller("WorksheetCarMileageCtrl",["$scope",
     "ionicMaterialInk",
     "ionicMaterialMotion",
     "$ionicPopup", "$timeout","$state","worksheetDataService", function($scope, ionicMaterialInk,ionicMaterialMotion,$ionicPopup,$timeout,$state,worksheetDataService){
-    ionicMaterialInk.displayEffect();
+        $scope.$on("$stateChangeSuccess", function (event, toState, toParams, fromState, fromParam){
+            if(fromState.name == 'worksheetCarMileageEdit' && toState.name == 'worksheetCarMileage'){
+                var worksheetDetail = worksheetDataService.wsDetailData;
+                console.log(angular.toJson(worksheetDetail));
+            }
+        });
+        ionicMaterialInk.displayEffect();
         $scope.edit = function(){
             $state.go("worksheetCarMileageEdit");
         }
         var worksheetDetail = worksheetDataService.wsDetailData;
-        $scope.carMile = worksheetDetail.ET_MILEAGE.item;
-        console.log(angular.toJson($scope.carMile.MILEAGE_COUNTER));
+        console.log(angular.toJson(worksheetDetail));
+        $scope.carMile = worksheetDetail.ET_MILEAGE.item[0];
+        console.log(angular.toJson($scope.carMile));
         
 }]);
 
 worksheetModule.controller("WorksheetCarMileageEditCtrl",["$scope",
     "ionicMaterialInk",
     "ionicMaterialMotion",
-    "$ionicPopup", "$timeout","$state","worksheetDataService", function($scope, ionicMaterialInk,ionicMaterialMotion,$ionicPopup,$timeout,$state,worksheetDataService){
+    "$ionicPopup", "$timeout","$state","worksheetDataService",'HttpAppService','Prompter', function($scope, ionicMaterialInk,ionicMaterialMotion,$ionicPopup,$timeout,$state,worksheetDataService,HttpAppService,Prompter){
         ionicMaterialInk.displayEffect();
-        $scope.keep = function(){
-            $state.go("worksheetCarMileage");
-        }
         var worksheetDetail = worksheetDataService.wsDetailData;
-        $scope.carMile = worksheetDetail.ET_MILEAGE.item;
+        $scope.carMile = worksheetDetail.ET_MILEAGE.item[0];
+        console.log(angular.toJson($scope.carMile));
+        $scope.update = {
+            readDate : $scope.carMile.MILEAGE_DATE,
+            readValue : $scope.carMile.MILEAGE_VALUE,
+            readDescription : $scope.carMile.MILEAGE_DESC
+        };
+
+        $scope.keep = function(){
+            console.log(angular.toJson($scope.update));
+            Prompter.showLoading("正在提交");
+            var data={
+                "I_SYSTEM": { "SysName": ROOTCONFIG.hempConfig.baseEnvironment },
+                "IS_AUTHORITY": { "BNAME": worksheetDetail.ES_OUT_LIST.CREATED_BY },
+                "IS_OBJECT_ID": worksheetDetail.ydWorksheetNum,
+                "IS_PROCESS_TYPE": worksheetDetail.IS_PROCESS_TYPE,
+                "IT_MILEAGE": {
+                    "item": [
+                        {
+                            "MILEAGE_VALUE": $scope.update.readValue,
+                            "MILEAGE_DATE": $scope.update.readDate,
+                            "MILEAGE_DESC": $scope.update.readDescription
+                        }
+                    ]
+                }};
+            console.log(angular.toJson(data));
+            var url = ROOTCONFIG.hempConfig.basePath + 'SERVICE_CHANGE';
+            HttpAppService.post(url, data).success(function(response){
+                worksheetDataService.wsDetailData.ET_MILEAGE.item[0].MILEAGE_VALUE = $scope.update.readValue
+                worksheetDataService.wsDetailData.ET_MILEAGE.item[0].MILEAGE_DATE = $scope.update.readDate
+                worksheetDataService.wsDetailData.ET_MILEAGE.item[0].MILEAGE_DESC =  $scope.update.readDescription
+                Prompter.hideLoading();
+                $state.go("worksheetCarMileage");
+                console.log(angular.toJson(response));
+            }).error(function(err){
+                console.log(angular.toJson(err));
+            });
+        }
     }]);
