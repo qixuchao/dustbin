fa68878eaf379f57f353f903546bd584dcc31ffd
xueboren <xueboren001@outlook.com>
2016-05-09 17:31:34 +0800

    reset

---
+++ www/src/worksheet/baoGong/baoGongModule.js
@@ -0,0 +1,22 @@
+worksheetReportModule.service("baoGongService", [function(){
+	
+	return {
+
+		detailFromWSDetail: {}, /*{
+			IS_OBJECT_ID: ,
+			IS_PROCESS_TYPE: ,
+			TYPE_DESC: ,
+			DESCRIPTION: ,
+			STATU: ,
+			STATU_DESC: ,
+			WAIFU_EMP: {
+				FCT_DESCRIPTION : "外服人员",
+				NAME1 : "张建廷",
+				PARTNER_FCT : "ZSRVEMPL",
+				PARTNER_NO : "E060000878"
+			}
+		}*/
+
+	};
+
+}]);
