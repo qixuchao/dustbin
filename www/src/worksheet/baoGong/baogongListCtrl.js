53a7adc3f33030223e283b20aacb0f70cc6f04a9
xueboren <xueboren001@outlook.com>
2016-05-12 15:55:39 +0800

    添加销售线索和活动计划模块

--- www/src/worksheet/baoGong/baogongListCtrl.js
+++ www/src/worksheet/baoGong/baogongListCtrl.js
@@ -3,7 +3,8 @@ worksheetModule.controller("WorksheetBaoGongListCtrl",[
     "ionicMaterialInk",
     "ionicMaterialMotion",
     "worksheetDataService",
-    function($scope, ionicMaterialInk, ionicMaterialMotion, worksheetDataService){
+    "Prompter",
+    function($scope, ionicMaterialInk, ionicMaterialMotion, worksheetDataService, Prompter){
     
     //alert(" ---- WorksheetBaoGongListCtrl ---- ");
     
@@ -25,6 +26,24 @@ worksheetModule.controller("WorksheetBaoGongListCtrl",[
         $scope.config.isEditPrice = false;
         $scope.config.isEditDetail = true;
     };
+    
+    $scope.editCancel = function(){
+        Prompter.wsConfirmFuc("提示", "确实退出编辑?", "确定", "取消", function(){
+            $scope.config.isEditPrice = false;
+            $scope.config.isEditDetail = false;
+        }, function(){
+        });
+    };
+
+    $scope.editOk = function(){
+        if($scope.config.isEditDetail){
+            __requestSaveDetail();
+        }else if($scope.config.isEditPrice){
+            __requestSavePrice();
+        }
+    };
+
+
     
     $scope.datas = {
         baogongDatas: [],
@@ -53,8 +72,17 @@ worksheetModule.controller("WorksheetBaoGongListCtrl",[
     	]
     };
 
+    $scope.detailLowerOne = function(item){
+        if(item.NUMBER_INT_XBR > 0){
+            item.NUMBER_INT_XBR--;
+        }
+    };
+
+    $scope.detailAddOne = function(item){
+        item.NUMBER_INT_XBR++;
+    };
     
-    $scope.init = function(){
+    $scope.init = function(){ //NUMBER_INT_XBR
         if(worksheetDataService.wsBaoDetailToFYJS){
             $scope.config.isBaoWS = true;
             worksheetDataService.wsBaoDetailToFYJS = false;
@@ -73,9 +101,63 @@ worksheetModule.controller("WorksheetBaoGongListCtrl",[
                 $scope.config.noDatas = true;
             }
         }
-        
-        
+        for(var i = 0; i < $scope.datas.baogongDatas.length; i++){
+            $scope.datas.baogongDatas[i].NUMBER_INT_XBR = window.parseInt($scope.datas.baogongDatas[i].NUMBER_INT);
+        }
     };
     $scope.init();
+
+
+    //baoGongService.BAOWS_EDIT.
+    function __requestSaveDetail(){
+        var url = baoGongService.BAOWS_EDIT.url;
+        var params = angular.copy(baoGongService.BAOWS_EDIT.defaults);
+        params.IT_DETAIL = {
+            item_in: [
+
+            ]
+        };
+        var itemIns = [];
+        for(var i = 0; i < $scope.datas.baogongDatas.length; i++){
+            var tempItem = $scope.datas.baogongDatas[i];
+            if(tempItem.NUMBER_INT_XBR != Number(tempItem.NUMBER_INT)){
+                itemIns.push({
+
+                });
+            }
+        }
+
+        Prompter.showLoading("正在修改");
+        var promise = HttpAppService.post(url,params);
+        promise.success(function(response){
+            if(response && response.ES_RESULT && response.ES_RESULT.ZFLAG == "S"){
+                Prompter.showLoadingAutoHidden("修改成功", false, 1000);
+                $timeout(function(){
+                    $scope.config.isEditPrice = false;
+                    $scope.config.isEditDetail = false;
+                }, 1000);
+            }else if(response && response.ES_RESULT && response.ES_RESULT.ZRESULT != ""){
+                Prompter.showLoadingAutoHidden(response.ES_RESULT.ZRESULT, false, 2000);
+            }else{
+                Prompter.showLoadingAutoHidden(response, false, 2000);
+            }
+        })
+        .error(function(errorResponse){
+            Prompter.showLoadingAutoHidden("修改失败,请检查网络!", false, 2000);
+        });
+    }
+
+    function __requestSavePrice(){
+
+    }
+
+
+
+
+
+
+
+
+
     
 }]);
