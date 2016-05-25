875b2fbfc5b05a2d0af786b7bc8138a2a0ce9e05
xueboren <xueboren001@outlook.com>
2016-05-25 15:31:33 +0800

    before pull

--- www/src/signin/create/signinCreateCtrls.js
+++ www/src/signin/create/signinCreateCtrls.js
@@ -5,4 +5,38 @@ signinModule.controller('signinCreateCtrl', [
 	"$state",
 	function ($scope, $timeout, signinService, $state) {
 
+	$scope.config = {
+		address: '',
+		signInDate: '',
+		signInTime: '',
+		comment: ''
+	};
+
+	$scope.datas = {
+
+	};
+
+	$scope.init = function(){
+		$scope.config.signInDate = '';
+		$scope.config.signInTime = '';
+	};
+	$scope.init();
+
+	$scope.saveNewSingin = function(){
+		var options = {
+		    "person_code":"60000051",
+		    "person_name":"刘 楚兴",
+		    "address": $scope.config.address,
+		    "sign_in_date": $scope.config.signInDate,
+		    "sign_in_time": $scope.config.signInTime,
+		    "comment": $scope.config.comment
+		};
+		__requestSaveNewSingin(options);
+	};
+
+	function __requestSaveNewSingin(options){
+
+	}
+
+
 }]);
